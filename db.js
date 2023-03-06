dd06cff71750c440eda08c9b51a7f19faee96fec
maxim.lisak <maxim.lisak@mobilitygroup.net>
2023-03-06 14:25:14 +0300

    db

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const {Sequelize} = require('sequelize');
 
 module.exports = new Sequelize(
-    'postgres',
-    'postgres',
+    'postgres1',//'postgres',
+    'postgres1',//'postgres',
     'root',
     {
-        host: 'localhost',
+        host: '5.188.76.226',// host: 'localhost',
+        port: '6432',
         dialect: 'postgres'
     }
 )
